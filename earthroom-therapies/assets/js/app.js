01823f0e11d3dd725bc57997e07596f5b8b24520
e0xaipd <dlrcac1@outlook.com>
2026-03-01 12:33:20 -0500

    Delete earthroom-therapies directory

--- earthroom-therapies/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-
-const btn = document.querySelector('.hamburger');
-const mobile = document.querySelector('.mobile-menu');
-if(btn){
-  btn.addEventListener('click',()=>{
-    const visible = mobile.style.display === 'block';
-    mobile.style.display = visible ? 'none' : 'block';
-  });
-}
